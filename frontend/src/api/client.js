ae9b5fd425f8eceab01637f3ef09ff93c832475f
unknown <sawssanatiq@gmail.com>
2025-05-20 01:47:46 +0100

    fina l profile

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,16 @@
 import api from "./api";
 
+export const getClient = async (id) => {
+  const response = await api.get(`/client/${id}`);
+  return response.data;
+};
 
-export const editClient = async (id) => {
-  const response = await api.put(`/user/${id}`);
+export const fetchClients = async () => {
+  const response = await api.get('/client');
+  return response.data;
+};
+
+export const updateClient = async (id,updatedData) => {
+  const response = await api.put(`/client/${id}`,updatedData);
   return response.data;
 };
